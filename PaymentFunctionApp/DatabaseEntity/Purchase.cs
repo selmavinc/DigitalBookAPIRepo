59d9cdeeb6dde901f5749f9d1c8d3f218bf50d95
unknown <cogdotnet1686@iiht.tech>
2022-09-21 02:24:47 +0530

    'paymentazurefunction'

---
+++ PaymentFunctionApp/DatabaseEntity/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentFunctionApp.DatabaseEntity
+{
+    public partial class Purchase
+    {
+        public int PurchaseId { get; set; }
+        public string EmailId { get; set; }
+        public int? BookId { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public string PaymentMode { get; set; }
+
+        public virtual Book Book { get; set; }
+    }
+}
